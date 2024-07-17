bf9cc4ada20f4af59f11d78f84040d3e7946012e
DamodarPatil <patildamodar1983@gmail.com>
2024-07-17 15:05:53 +0530

    chore: Add JavaScript 30-day challenge code for days 1-4

---
+++ JS_30D_CHALLENGE/DAY_1.js
@@ -0,0 +1,73 @@
+//Activity - 1
+
+//Task - 1
+var num = 83;
+console.log(num); 
+
+//Task - 2
+let str = "Damodar";
+console.log(str);
+
+//Activity - 2
+
+//Task - 3
+const isCorrect = false;
+console.log(isCorrect);
+
+//Activity - 3
+
+//Task - 4
+const myNum = 83;
+console.log("My favorite number is:", myNum);
+
+const myName = "Damodar";
+console.log("My name is:", myName);
+
+const isAwesome = true;
+console.log("Is JavaScript awesome?", isAwesome);
+
+const vermithor = {
+  name: "Vermithor",
+  nickname: "The Bronze Fury",
+  rider: "King Jaehaerys I Targaryen",
+  color: "Bronze",
+  size: "Very large",
+};
+console.log(vermithor);
+
+const Dragons = [
+  "Syrax",
+  "Caraxes",
+  "Meleys",
+  "Seasmoke",
+  "Vhagar",
+  "Arrax",
+  "Vermax",
+  "Sunfyre",
+  "Dreamfyre",
+  "Tessarion",
+  "Moondancer",
+  "Vermithor",
+  "Silverwing",
+];
+
+console.log(Dragons[11]);
+
+//Activity - 4
+
+//Task - 5
+let dragonName = "Moondancer";
+console.log(dragonName);
+dragonName = "Vermithor";
+console.log(dragonName);
+
+//Activity - 5
+
+//Task - 6
+const riderName = "Aemond Targaryen";
+console.log(riderName);
+try {
+  riderName = "King Jaehaerys";
+} catch (error) {
+  console.log(error.message);
+}
